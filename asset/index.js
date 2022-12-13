f27c90ff9c3b68343890f2521de108c261de1624
Bharatichaudhary <bharatichaudhary85@gmail.com>
2022-12-13 19:26:00 +0545

    updated

--- asset/index.js
+++ asset/index.js
@@ -6,4 +6,13 @@ function openNav() {
     document.getElementById("mySidepanel").style.width = "0";
   }
 
+
+function openSearch() {
+  document.getElementById("myOverlay").style.display = "block";
+}
+
+function closeSearch() {
+  document.getElementById("myOverlay").style.display = "none";
+}
+
   
